6210cec276b601566b555cb52c28ee9b01a44a40
Idoko Agbo <idokojudge@gmail.com>
2023-02-12 11:21:28 -0500

    add database connection and handle http requests

--- app.js
+++ app.js
@@ -1,22 +1,84 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://chat-server-user:jvZclyYOnZaR1DDY@idokoagbo1.1iuva.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+// import user schema
+const User = require('./schema/user');
+// import message schema
+const Message = require('./schema/message');
+// import response schema
+const Response = require('./schema/response');
+const { response } = require('express');
+
 app.use(cors());
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
     cors: {
         origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST", "PATCH"]
     }
 });
 
 app.use(express.static(__dirname + '/public'));
 
-const responses = {
-    'hello': 'hi',
-    'whats todays date': 'today is the 6th of february 2023',
-}
+// login
+app.post('/login', (req, res, next) => {
 
+    const email = req.body.username.toLowerCase();
+    const password = req.body.password;
+
+    User.find({
+        email: email,
+        password: password,
+    }, (error, user) => {
+        if (error) {
+            return res.status(500).send(error);
+        } else {
+            if (user.length > 0) {
+                return res.send(user[0]);
+            }
+
+            return res.status(400).send('Incorrect login credentials');
+        }
+    })
+
+});
+
+// get all responses
+app.get('/responses', (req, res, next) => {
+
+    Response.find({}, (error, document) => {
+        if (error) {
+            return res.status(500).send(error);
+        }
+
+        return res.send(document);
+    });
+
+});
+
+// update message response
+app.patch('/response/:id', (req, res, next) => {
+    const id = req.params.id;
+    const response = req.body.response;
+
+    Response.findById({ _id: id }, (error, document) => {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        else {
+            document.response = response;
+            document.save();
+
+            return res.send(document);
+        }
+    });
+});
+
+
+// handle communication
 io.on('connection', (socket) => {
     console.log('a user connected');
 
@@ -25,14 +87,30 @@ io.on('connection', (socket) => {
     });
 
     socket.on('message', (msg) => {
+        const text = msg.split(":");
+        const sender = text[0];
+        const body = text.pop();
         // save message to db
-        if (responses[msg]) {
-            // io.emit('message', `client: ${msg}`);
-            io.emit('message', `Server: ${responses[msg]}`);
+        const message = new Message({
+            message: body,
+            sender: sender,
+            receiver: sender == 'User' ? 'Agent' : 'User',
+        });
+        message.save();
 
-            return;
-        }
-        io.emit('message', `client said: ${msg}`);
+        // check for matching response
+        Response.find({ message: body }, (error, document) => {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                if (document.length > 0) {
+                    io.emit('message', `Agent: ${document[0].response}`);
+                } else {
+                    io.emit('message', `${msg}`);
+                }
+            }
+        });
     });
 });
 
